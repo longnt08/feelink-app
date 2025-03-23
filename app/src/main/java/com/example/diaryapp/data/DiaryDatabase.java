a5871ba13ab02398b7fe880d5e1b48f176e05249
longnt08 <thanhlong0j@gmail.com>
2025-03-23 22:53:20 +0700

    feat(ui): tao thiet ke trang chu

--- app/src/main/java/com/example/diaryapp/data/DiaryDatabase.java
+++ app/src/main/java/com/example/diaryapp/data/DiaryDatabase.java
@@ -1,21 +1,20 @@
 package com.example.diaryapp.data;
 
 import android.content.Context;
-import android.nfc.Tag;
 
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
-import com.example.diaryapp.data.dao.EntryDao;
-import com.example.diaryapp.data.dao.SettingsDao;
-import com.example.diaryapp.data.dao.UserDao;
-import com.example.diaryapp.data.entities.Entry;
-import com.example.diaryapp.data.entities.EntryMood;
-import com.example.diaryapp.data.entities.Media;
-import com.example.diaryapp.data.entities.Mood;
-import com.example.diaryapp.data.entities.Settings;
-import com.example.diaryapp.data.entities.User;
+import com.example.diaryapp.data.local.dao.EntryDao;
+import com.example.diaryapp.data.local.dao.SettingsDao;
+import com.example.diaryapp.data.local.dao.UserDao;
+import com.example.diaryapp.data.local.entities.Entry;
+import com.example.diaryapp.data.local.entities.EntryMood;
+import com.example.diaryapp.data.local.entities.Media;
+import com.example.diaryapp.data.local.entities.Mood;
+import com.example.diaryapp.data.local.entities.Settings;
+import com.example.diaryapp.data.local.entities.User;
 
 @Database(entities = {User.class, Settings.class, Entry.class, Media.class, Mood.class, EntryMood.class}, version = 2)
 public abstract class JournalDatabase extends RoomDatabase {
